3a4720a10c8746fe4edaf7cfa809f17defd9aa1e
arbyun <someonealiveh@gmail.com>
2023-10-24 15:36:38 +0100

    add more catch clauses to the code

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -6,17 +6,24 @@ namespace ExceptionTest
     {
         static void Main(string[] args)
         {
-            Console.Write("Insere um numero inteiro:");
+            Console.Write("Insere um número inteiro:");
 
             try
             {
                 int i = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine($"Numero inserido: {i}");
+                Console.WriteLine($"Número inserido: {i}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Tens que inserir um número inteiro.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Este número é grande demais.");
             }
             catch (Exception e)
             {
                 Console.WriteLine("Ocorreu o seguinte problema: " + e.Message);
-                throw;
             }
         }
     }
