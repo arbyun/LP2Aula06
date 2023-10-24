b1a20b05d56e622e86f9284a41294655c5844415
arbyun <someonealiveh@gmail.com>
2023-10-24 14:34:52 +0100

    serialized int _myInt

--- Attrib/Assets/MyScript.cs
+++ Attrib/Assets/MyScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MyScript : MonoBehaviour
 {
-    private int _myInt;
+    [SerializeField] private int _myInt;
     
     // Start is called before the first frame update
     void Start()
